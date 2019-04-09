c9fea3978041468d70b5cb7e6d4c4838dd204031
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-04-09 03:40:28 -0300

    Improve Test case fetching
    - Now it uses a custom query that uses joins with fetches (this avoids 2N + 1 queries, as the element collections are retrieved at once).

--- evaluations-service-adapters/evaluations-service-spring-data-adapter/src/main/java/ar/edu/itba/cep/evaluations_service/spring_data/interfaces/SpringDataTestCaseRepository.java
+++ evaluations-service-adapters/evaluations-service-spring-data-adapter/src/main/java/ar/edu/itba/cep/evaluations_service/spring_data/interfaces/SpringDataTestCaseRepository.java
@@ -3,7 +3,9 @@ package ar.edu.itba.cep.evaluations_service.spring_data.interfaces;
 import ar.edu.itba.cep.evaluations_service.models.Exam;
 import ar.edu.itba.cep.evaluations_service.models.Exercise;
 import ar.edu.itba.cep.evaluations_service.models.TestCase;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,10 +20,18 @@ public interface SpringDataTestCaseRepository extends CrudRepository<TestCase, L
      * Retrieves the {@link TestCase}s belonging to the given {@code exercise}, applying visibility filter.
      *
      * @param exercise   The {@link Exercise} owning the {@link TestCase}s being returned.
-     * @param visibility The {@link TestCase.Visibility} of the retrived {@link TestCase}s.
+     * @param visibility The {@link TestCase.Visibility} of the retrieved {@link TestCase}s.
      * @return The {@link TestCase}s belonging to the given {@code exercise}, with the given {@code visibility}.
      */
-    List<TestCase> getByBelongsToAndVisibility(final Exercise exercise, final TestCase.Visibility visibility);
+    @Query(value = "SELECT DISTINCT tc " +
+            "       FROM TestCase tc " +
+            "           LEFT JOIN FETCH tc.inputs " +
+            "           LEFT JOIN FETCH tc.expectedOutputs " +
+            "       WHERE tc.belongsTo = :exercise AND tc.visibility = :visibility")
+    List<TestCase> getByBelongsToAndVisibility(
+            @Param("exercise") final Exercise exercise,
+            @Param("visibility") final TestCase.Visibility visibility
+    );
 
 
     /**
