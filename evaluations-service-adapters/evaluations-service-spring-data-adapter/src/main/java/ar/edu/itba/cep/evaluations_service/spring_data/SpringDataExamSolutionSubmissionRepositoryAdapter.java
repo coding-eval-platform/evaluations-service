b2abd42f1d67fb45a2bbe4ed41064c8ec4c8999d
Juan Marcos Bellini <juan.bellini@wolox.com.ar>
2019-08-22 01:47:20 -0300

    Fix ExamSolutionSubmission repository
    - Make the get by exam method return a page
    - Add an exists by exam and submitter

--- evaluations-service-adapters/evaluations-service-spring-data-adapter/src/main/java/ar/edu/itba/cep/evaluations_service/spring_data/SpringDataExamSolutionSubmissionRepositoryAdapter.java
+++ evaluations-service-adapters/evaluations-service-spring-data-adapter/src/main/java/ar/edu/itba/cep/evaluations_service/spring_data/SpringDataExamSolutionSubmissionRepositoryAdapter.java
@@ -5,12 +5,14 @@ import ar.edu.itba.cep.evaluations_service.models.ExamSolutionSubmission;
 import ar.edu.itba.cep.evaluations_service.repositories.ExamSolutionSubmissionRepository;
 import ar.edu.itba.cep.evaluations_service.spring_data.interfaces.SpringDataExamSolutionSubmissionRepository;
 import ar.edu.itba.cep.evaluations_service.spring_data.interfaces.SpringDataExerciseRepository;
+import com.bellotapps.webapps_commons.persistence.repository_utils.paging_and_sorting.Page;
+import com.bellotapps.webapps_commons.persistence.repository_utils.paging_and_sorting.PagingRequest;
+import com.bellotapps.webapps_commons.persistence.spring_data.repository_utils_adapters.paging_and_sorting.PagingMapper;
 import com.bellotapps.webapps_commons.persistence.spring_data.repository_utils_adapters.repositories.BasicRepositoryAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -54,8 +56,10 @@ public class SpringDataExamSolutionSubmissionRepositoryAdapter
     // ================================================================================================================
 
     @Override
-    public List<ExamSolutionSubmission> getByExam(final Exam exam) {
-        return repository.getByExam(exam);
+    public Page<ExamSolutionSubmission> getByExam(final Exam exam, final PagingRequest pagingRequest) {
+        final var pageable = PagingMapper.map(pagingRequest);
+        final var page = repository.getByExam(exam, pageable);
+        return PagingMapper.map(page);
     }
 
     @Override
@@ -66,5 +70,10 @@ public class SpringDataExamSolutionSubmissionRepositoryAdapter
     @Override
     public Optional<ExamSolutionSubmission> getSubmissionFor(final Exam exam, final String submitter) {
         return repository.getByExamAndSubmitter(exam, submitter);
+    }
+
+    @Override
+    public boolean existsSubmissionFor(final Exam exam, final String submitter) {
+        return repository.existsByExamAndSubmitter(exam, submitter);
     }
 }
--- evaluations-service-core/evaluations-service-ports/evaluations-service-repositories/src/main/java/ar/edu/itba/cep/evaluations_service/repositories/ExamSolutionSubmissionRepository.java
+++ evaluations-service-core/evaluations-service-ports/evaluations-service-repositories/src/main/java/ar/edu/itba/cep/evaluations_service/repositories/ExamSolutionSubmissionRepository.java
@@ -2,9 +2,10 @@ package ar.edu.itba.cep.evaluations_service.repositories;
 
 import ar.edu.itba.cep.evaluations_service.models.Exam;
 import ar.edu.itba.cep.evaluations_service.models.ExamSolutionSubmission;
+import com.bellotapps.webapps_commons.persistence.repository_utils.paging_and_sorting.Page;
+import com.bellotapps.webapps_commons.persistence.repository_utils.paging_and_sorting.PagingRequest;
 import com.bellotapps.webapps_commons.persistence.repository_utils.repositories.BasicRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -13,12 +14,15 @@ import java.util.Optional;
 public interface ExamSolutionSubmissionRepository extends BasicRepository<ExamSolutionSubmission, Long> {
 
     /**
-     * Retrieves the {@link ExamSolutionSubmission}s belonging to the given {@code exam}.
+     * Retrieves the {@link ExamSolutionSubmission}s belonging to the given {@code exam},
+     * in a {@link Page} representation, according to the given {@code pagingRequest}.
      *
-     * @param exam The {@link Exam} owning the {@link ExamSolutionSubmission}s being returned.
-     * @return The {@link ExamSolutionSubmission}s belonging to the given {@code exam}.
+     * @param exam          The {@link Exam} owning the {@link ExamSolutionSubmission}s being returned.
+     * @param pagingRequest The {@link PagingRequest} that indicates page number, size, sorting options, etc.
+     * @return A {@link Page} with the {@link ExamSolutionSubmission} belonging to the given {@code exam},
+     * configured according to the given {@code pagingRequest}.
      */
-    List<ExamSolutionSubmission> getByExam(final Exam exam);
+    Page<ExamSolutionSubmission> getByExam(final Exam exam, final PagingRequest pagingRequest);
 
     /**
      * Deletes the {@link ExamSolutionSubmission}s belonging to the given {@code exam}.
@@ -36,4 +40,13 @@ public interface ExamSolutionSubmissionRepository extends BasicRepository<ExamSo
      * or empty otherwise.
      */
     Optional<ExamSolutionSubmission> getSubmissionFor(final Exam exam, final String submitter);
+
+    /**
+     * Indicates whether an {@link ExamSolutionSubmission} exists for the given {@code exam} and {@code submitter}.
+     *
+     * @param exam      The {@link Exam} to be checked.
+     * @param submitter The submitter.
+     * @return {@code true} if a matching {@link ExamSolutionSubmission} exists, or {@code false} otherwise.
+     */
+    boolean existsSubmissionFor(final Exam exam, final String submitter);
 }
